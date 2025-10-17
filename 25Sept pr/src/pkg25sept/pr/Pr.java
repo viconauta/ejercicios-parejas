8362d5f20c6b23414083919ffa13909aa6e643b2
pablosierra23 <pablosg1008@gmail.com>
2025-10-17 09:43:53 +0200

    Commit Pablo2

--- 25Sept pr/src/pkg25sept/pr/Pr.java
+++ 25Sept pr/src/pkg25sept/pr/Pr.java
@@ -31,11 +31,11 @@ public class Pr {
             System.out.println(num + " / " + num2 + " = " + division + " Resto: " + resto);
             System.out.println("El resto de " + num + " / " + num2 + " = " + resto);
             
-            System.out.println("Cambio ejercicio 40 alumno a1");
-            System.out.println("Cambio de Alumno A");
-            System.out.println("Cambio de Alumno B");
-           
+            System.out.println("Primera linea");
+            
+            
        
     }
     
 }
+
